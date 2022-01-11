66e780d1915a874bf5f4dc8740d124bed83c1da8
francomartin-github <francomartinmdp@gmail.com>
2022-01-11 15:42:08 -0300

    Upload

---
+++ js/main.js
@@ -0,0 +1,89 @@
+const input = document.querySelector("input"); //Entrada formulario
+const addBtn = document.querySelector("#add-button"); //Botón agregar tarea
+const list = document.querySelector("#todo-list");  //Lista de tareas
+const empty = document.querySelector(".empty");  //Mensaje de lista vacía
+const full = document.querySelector(".full");
+
+full.style.display = "none";
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault(); //No recarga la página al hacer click en botón
+    const text = input.value;
+
+    if (text !== "") {
+        const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = text;
+
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    li.appendChild(addUpBtn());
+    li.appendChild(addDownBtn());
+    list.appendChild(li);
+
+    input.value = "";
+    empty.style.display = "none";
+    full.style.display = "block";
+    }
+    
+});
+
+function addDeleteBtn() {
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "btn-delete";
+
+    deleteBtn.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+        list.removeChild(item);
+
+        const items = document.querySelectorAll("li");
+        if (items.length == 0) {
+            empty.style.display="block";
+            full.style.display= "none";
+        }
+    })
+    return deleteBtn;
+}
+
+function addUpBtn() {
+    const upBtn = document.createElement('button');
+    upBtn.textContent = "^";
+    upBtn.className = "btn-up";
+
+    upBtn.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+        
+        if (item.previousElementSibling) {
+            item.parentNode.insertBefore(item, item.previousElementSibling);
+        }
+
+    })
+    return upBtn;
+}
+
+function addDownBtn() {
+    const downBtn = document.createElement('button');
+    downBtn.textContent = "v";
+    downBtn.className = "btn-up";
+
+    downBtn.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+        
+        if (item.nextElementSibling) {
+            item.parentNode.insertBefore(item.nextElementSibling, item);
+        }
+
+    })
+    return downBtn;
+}
+
+
+
+full.addEventListener ('click', (e) => {
+    var toDoItems = list.children;
+    while (toDoItems.length > 0) {
+        toDoItems.item(0).remove();
+    }
+    
+})
